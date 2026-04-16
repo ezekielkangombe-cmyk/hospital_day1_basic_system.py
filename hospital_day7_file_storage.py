9711a55e53819e8d57514dab203eed072eed40e6
Ezekiel Kang’ombe <ezekielkangombe@gmail.com>
2026-04-17 00:46:16 +0200

    Day 7: Implemented data persistence using JSON file storage
    Display the list of patients with their details.

---
+++ hospital_day7_file_storage.py
@@ -0,0 +1,31 @@
+--- Hospital System ---
+1. Add Patient
+2. View Patients
+3. Search Patient
+4. Delete Patient
+5. Update Patient
+6. Exit
+Enter choice: 2
+
+--- Patient List ---
+
+Patient 1:
+Name: josep maseko
+Age: 46
+Gender: male
+Condition: flu
+
+Patient 2:
+Name: gloria banda
+Age: 66
+Gender: female
+Condition: malaria
+
+--- Hospital System ---
+1. Add Patient
+2. View Patients
+3. Search Patient
+4. Delete Patient
+5. Update Patient
+6. Exit
+Enter choice:
